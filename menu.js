e5e1607673dbf1f75bf67550c5023b4d1da0b765
Portspeak <portspeak00@gmail.com>
2024-08-21 15:26:56 -0500

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,10 @@
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
+const dropDownMenu = document.querySelector('.dropdown_menu')
+
+toggleBtn.onclick = function(){
+    dropDownMenu.classList.toggle('open')
+    const iOpen = dropDownMenu.classList.contains('open')
+
+    
+}
